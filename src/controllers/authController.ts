0cc1376a11cc90dfc08be7e60aa152a927012acf
JCarlosrm <juanc.ramirezm16@gmail.com>
2024-03-24 19:30:44 -0600

    Login de usuario

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { hashPassword } from "../services/password.service";
+import { comparePasswords, hashPassword } from "../services/password.service";
 import prisma from "../models/user";
 import { generateToken } from "../services/auth.service";
 
@@ -7,8 +7,14 @@ export const register = async (req: Request, res: Response): Promise<void> => {
   const { email, password } = req.body;
 
   try {
-    if (!email) throw new Error("El email es obligatorio");
-    if (!password) throw new Error("El password es obligatorio");
+    if (!email) {
+      res.status(400).json({ error: "El email es obligatorio" });
+      return;
+    }
+    if (!password) {
+      res.status(400).json({ error: "El password es obligatorio" });
+      return;
+    }
     const hashedPassword = await hashPassword(password);
 
     const user = await prisma.create({
@@ -22,15 +28,38 @@ export const register = async (req: Request, res: Response): Promise<void> => {
     res.status(201).json({ token });
     console.log(token);
   } catch (error: any) {
-    if (!email) {
-      res.status(400).json({ error: "El email es obligatorio" });
-    }
-    if (!password) {
-      res.status(400).json({ error: "El password es obligatorio" });
-    }
     if (error?.code === "P2002" && error?.meta?.target?.includes("email")) {
       res.status(400).json({ error: "El email ingresado ya existe en bd" });
     }
     res.status(500).json({ error: "Hubo un error en el registro" });
+  }
+};
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email) {
+      res.status(400).json({ error: "El email es obligatorio" });
+      return;
+    }
+    if (!password) {
+      res.status(400).json({ error: "El password es obligatorio" });
+      return;
+    }
+    const user = await prisma.findUnique({ where: { email } });
+    if (!user) {
+      res.status(404).json({ error: "Usuario no encontrado" });
+    }
+    const pass = user?.password || "";
+    const id = user?.id || 0;
+    const passwordMatch = await comparePasswords(password, pass);
+    if (!passwordMatch) {
+      res.status(401).json({ error: "Usuario y contraseña no coinciden." });
+    }
+    const token = generateToken({ id, email });
+    res.status(200).json({ token });
+  } catch (error: any) {
+    console.log(error);
   }
 };
--- src/services/password.service.ts
+++ src/services/password.service.ts
@@ -7,3 +7,9 @@ export const hashPassword = async (password: string): Promise<string> => {
 };
 
 // Leer y comparar con el hash de la base de datos
+export const comparePasswords = async (
+  password: string,
+  hash: string
+): Promise<boolean> => {
+  return await bcrypt.compare(password, hash);
+};
